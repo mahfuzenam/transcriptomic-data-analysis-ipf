6e9f5afd70acc17cfa5c6572be32477b123f15ee
mahfuzenam <67872210+mahfuzenam@users.noreply.github.com>
2025-10-03 03:32:36 +0600

    Commited

---
+++ scripts/Protocol.R
@@ -0,0 +1,5 @@
+# Create organized folders
+dir.create("data")           # For raw data
+dir.create("results")        # For output files
+dir.create("figures")        # For plots
+dir.create("scripts")        # For R code
